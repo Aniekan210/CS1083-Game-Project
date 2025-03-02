e94633004d8e2be359544bb58cfdae26d20f55ed
Emmyk-17 <136089110+Emmyk-17@users.noreply.github.com>
2025-03-02 17:11:12 +0100

    Checked TotalPayoutScreen
    Checked by kings

--- TotalPayoutScreen.java
+++ TotalPayoutScreen.java
@@ -5,18 +5,19 @@ import javafx.scene.image.Image;
 import javafx.scene.text.Text;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-/**
- * Write a description of class TotalPayoutScreen here.
+/*****************************************************************
+ * This class shows the total number of vbucks won by the player
  *
- * @author (your name)
- * @version (a version number or a date)
- */
+ * @author CS Group Aniekan
+ * @version new feature
+ ****************************************************************/
 public class TotalPayoutScreen extends Pane
 {    
     protected Text payoutText;
     
     public TotalPayoutScreen()
     {
+        //Text showing the amount of vbucks won by the player so far
         super();
         payoutText = new Text("0");
         payoutText.setFont(Font.font("Comic Sans MS", 30));
@@ -25,7 +26,8 @@ public class TotalPayoutScreen extends Pane
         payoutText.setStrokeWidth(1);
         payoutText.setLayoutX(15);
         payoutText.setLayoutY(57);
-        
+
+        //place the text in the box
         ImageView bg = new ImageView(new Image("./assets/images/payout_screen.png"));
         bg.setPreserveRatio(true);
         bg.setFitWidth(150);
